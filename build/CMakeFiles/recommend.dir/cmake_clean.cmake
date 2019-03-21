a28d43c290570384b3f8e48dcb59cedeb650ac37
Cc-BJYG <48313587+Cc-BJYG@users.noreply.github.com>
2019-03-22 01:35:22 +0800

    build~
    build
    编译器：G++；

---
+++ build/CMakeFiles/recommend.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/recommend.dir/librecommend.cpp.obj"
+  "librecommend.pdb"
+  "librecommend.dll"
+  "librecommend.dll.a"
+  "librecommend.dll.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/recommend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/Element.cpp.obj"
+  "CMakeFiles/src.dir/Map.cpp.obj"
+  "CMakeFiles/src.dir/QSort.cpp.obj"
+  "libsrc.pdb"
+  "libsrc.dll"
+  "libsrc.dll.a"
+  "libsrc.dll.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
